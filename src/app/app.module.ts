e0627ddbab1ce28018648752f1ba4a4228275aab
Gilbertkitetu <gilbertkitetu2@gmail.com>
2022-10-18 14:23:17 +0300

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -34,6 +36,7 @@ import { ItemsService } from './services/ItemsService';
     FormsModule
   ],
   providers: [ItemsService],
+     
   bootstrap: [AppComponent]
 })
 export class AppModule { }
